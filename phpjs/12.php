06826f64e5bb94c99c2e03e079cfab4e80f3bf9c
zabastian <qlgh10gh@gmail.com>
2021-03-24 18:37:50 +0900

    Add 12.php File[12.php]

---
+++ phpjs/12.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset = "utf-8">
+</head>
+<body>
+  <h1>php</h1>
+  <?php
+  echo 10+10; //서버쪽에서 실행되는 언어이다. 서버에서 만들어져 웹 브라우저로 전송한다.
+// html 이나 css는 할수없는 방식이다.
+   ?>
+   <h1>javascript</h1>
+   <script>  //  <-html 태그이다.
+ 
+     document.write(10+10); // 웹브라우저(클라이언트)가 해석해서 처리하기 때문에 소스를 살펴보면 소스가 다 출력되는것을 알수있다.
+   </script>
+</body>
+</html>
