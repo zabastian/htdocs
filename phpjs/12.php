3c542579a29db9d9d229d2ff5fdc6bcac1a668f3
zavastian <qlgh10gh@gmail.com>
2021-03-25 20:42:44 +0900

    Add 12.php [12.php]

--- phpjs/12.php
+++ phpjs/12.php
@@ -4,15 +4,28 @@
     <meta charset = "utf-8">
 </head>
 <body>
-  <h1>php</h1>
-  <?php
-  echo 10+10; //서버쪽에서 실행되는 언어이다. 서버에서 만들어져 웹 브라우저로 전송한다.
-// html 이나 css는 할수없는 방식이다.
-   ?>
-   <h1>javascript</h1>
-   <script>  //  <-html 태그이다.
- 
-     document.write(10+10); // 웹브라우저(클라이언트)가 해석해서 처리하기 때문에 소스를 살펴보면 소스가 다 출력되는것을 알수있다.
-   </script>
+<h1> JavaScript </h1>
+<ul>
+<script>
+list = new Array("김현준","국경록");
+i = 0;
+while( i < list.length){
+  document.write("<li>"+list[i]+"</li>");
+  i = i + 1;
+} 
+</script>
+</ul>
+
+<h1>php</h1>
+<ul>
+<?php
+$list = array("김현준","아이유");
+$i=0;
+while($i < count($list)){
+  echo "<li>".$list[$i]."</li>";
+  $i = $i + 1;
+}
+?>
+</ul>
 </body>
 </html>
