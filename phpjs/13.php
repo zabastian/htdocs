d5e5fec98cd2449b6408d5b15a0661675f1b54f5
zabastian <qlgh10gh@gmail.com>
2021-03-27 21:32:19 +0900

    Add 13.php[13.php]

---
+++ phpjs/13.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+</head>
+
+<body>
+
+    <h1>Java</h1>
+    <script>
+        function a() {
+            document.write("hello javascript");
+        }
+        a();
+    </script>
+
+    <h1>Php</h1>
+    <?php
+    function a()
+    {
+        echo "Hello Php";
+    }
+    a();
+    ?>
+
+
+</body>
+
+</html>
+
+<!-- fuction()을 jacascrip와 php가 함수로 사용한다> -->
+
+<!-- 문법은 function a()와 같은 식이다. -->
