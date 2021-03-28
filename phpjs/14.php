a85e56edd46a472c6814cad7cae8c322704a8024
zabastian <qlgh10gh@gmail.com>
2021-03-28 23:31:26 +0900

    Add 14.php[14.php]

---
+++ phpjs/14.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+</head>
+
+<body>
+
+    <h1>JavaScript</h1>
+    <script>
+        function a(x) {
+           return x+1;
+        }
+        document.write(a(6));
+        prompt(a(6));
+    </script>
+
+    <h1>Php</h1>
+    <?php
+    function a($x)
+    {
+        return $x+1;
+    }
+    echo a(6);
+    ?>
+
+</body>
+</html>
+
+    <!-- a(x)는 함수이고 x 에 들어갈값이 리턴값으로 x+1로 들어가 7이 나온다. -->
